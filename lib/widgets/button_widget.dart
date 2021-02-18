872e958b51cbbb7052a7624dc4843777c649e34b
Richard Mbingi <mbingicoding@gmail.com>
2021-02-18 11:36:05 +0300

    Button animation improvements

--- lib/widgets/button_widget.dart
+++ lib/widgets/button_widget.dart
@@ -1,4 +1,5 @@
 import 'dart:math' as math;
+
 import 'package:flutter/material.dart';
 
 class CustomButton extends StatefulWidget {
@@ -12,45 +13,36 @@ class _CustomButtonState extends State<CustomButton>
     with SingleTickerProviderStateMixin {
   AnimationController _controller;
   Animation _animation;
-
   @override
   void initState() {
     super.initState();
-    _controller = AnimationController(
-      vsync: this,
-      duration: Duration(seconds: 1),
-    );
-    _animation = Tween(begin: 1.0, end: 1.2).animate(
-      CurvedAnimation(parent: _controller, curve: Curves.easeIn),
-    );
+    _controller =
+        AnimationController(vsync: this, duration: Duration(seconds: 1));
+    _animation = Tween(begin: 1.0, end: 1.2)
+        .animate(CurvedAnimation(parent: _controller, curve: Curves.easeIn));
     _controller.repeat(reverse: true);
   }
 
   @override
   Widget build(BuildContext context) {
     return AnimatedBuilder(
-      animation: _controller,
-      builder: (context, widget) {
-        return Transform.rotate(
-          angle: _controller.status == AnimationStatus.forward
-              ? (math.pi * 2) * _controller.value
-              : -(math.pi * 2) * _controller.value,
-          child: Container(
-            height: 90.0,
-            width: 90.0,
-            child: CustomPaint(
-              painter: ButtonCanvas(radius: _animation.value),
+        animation: _controller,
+        builder: (context, widget) {
+          return Transform.rotate(
+            angle: _controller.status == AnimationStatus.forward
+                ? (math.pi * 2) * _controller.value
+                : -(math.pi * 2) * _controller.value,
+            child: Container(
+              height: 55,
+              width: 55,
+              child: CustomPaint(
+                painter: ButtonCanvas(
+                  radius: _animation.value,
+                ),
+              ),
             ),
-          ),
-        );
-      },
-    );
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
+          );
+        });
   }
 }
 
@@ -72,6 +64,18 @@ class ButtonCanvas extends CustomPainter {
     Offset _center = Offset(size.width / 2, size.height / 2);
 
     canvas.drawCircle(_center, size.width / 2, _circle);
+
+    canvas.drawArc(
+      Rect.fromCenter(
+        center: _center,
+        width: size.width * radius,
+        height: size.height * radius,
+      ),
+      math.pi / 4,
+      math.pi / 2,
+      false,
+      _arc,
+    );
     canvas.drawArc(
       Rect.fromCenter(
         center: _center,
@@ -86,7 +90,7 @@ class ButtonCanvas extends CustomPainter {
   }
 
   @override
-  bool shouldRepaint(ButtonCanvas oldPaint) {
+  bool shouldRepaint(CustomPainter oldDelegate) {
     return true;
   }
 }
