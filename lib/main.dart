e4f2b4f75a6476139c8c19dcd73a6b63dbe996b7
Richard Mbingi <mbingicoding@gmail.com>
2021-02-23 17:41:37 +0300

    Palette generator implemented

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,13 @@ void main() {
   runApp(MyApp());
 }
 
+//TODO: Optimize memory use 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:funiture_list/data/furniture_list.dart';
+import 'package:funiture_list/models/furniture_model.dart';
 import 'package:funiture_list/widgets/button_widget.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:palette_generator/palette_generator.dart';
@@ -13,50 +14,80 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   List<PaletteColor> colors;
+  final  int _currentIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    colors = [];
+    _updatePalettes();
+  }
+
+//TODO: Images not being referenced 
+  _updatePalettes() async {
+    for (Furniture furnitureImg in furnitureList) {
+      final PaletteGenerator generator =
+          await PaletteGenerator.fromImageProvider(
+        AssetImage(furnitureImg.toString()),
+      );
+
+      colors.add(
+        generator.lightMutedColor != null
+            ? generator.lightMutedColor
+            : PaletteColor(Colors.blue, 2),
+      );
+    }
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Container(
-            margin: EdgeInsets.fromLTRB(25.0, 0.0, 25.0, 20.0),
-            width: double.infinity,
-            height: 700,
-            child: PageView(
-              children: furnitureList
-                  .map((furniture) => Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Image.asset(furniture.furnitureImage),
-                          SizedBox(height: 80),
-                          Text(
-                            //TODO: Generate List Numbers
-                            ".01",
-                            style: GoogleFonts.questrial(
-                                fontSize: 35.0, fontWeight: FontWeight.w800),
-                          ),
-                          SizedBox(height: 20.0),
-                          Text(
-                            furniture.furnitureName,
-                            style: GoogleFonts.questrial(
-                                fontSize: 28.0, fontWeight: FontWeight.w500,),
-                          ),
-                          SizedBox(height: 5.0),
-                          Text(
-                            furniture.furnitureDescription,
-                            style: GoogleFonts.questrial(
-                              fontSize: 16.0,
-                              color: Colors.grey,
+      body: Container(
+        padding: EdgeInsets.fromLTRB(25.0, 0.0, 25.0, 20.0),
+        color: colors.isNotEmpty ? colors[_currentIndex].color : Colors.yellow[400],
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              height: 700,
+              child: PageView(
+                children: furnitureList
+                    .map((furniture) => Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Image.asset(furniture.furnitureImage),
+                            SizedBox(height: 60),
+                            Text(
+                              //TODO: Generate List Numbers
+                              ".01",
+                              style: GoogleFonts.questrial(
+                                  fontSize: 40.0, fontWeight: FontWeight.w800),
                             ),
-                          )
-                        ],
-                      ))
-                  .toList(),
+                            SizedBox(height: 20.0),
+                            Text(
+                              furniture.furnitureName,
+                              style: GoogleFonts.questrial(
+                                fontSize: 28.0,
+                                fontWeight: FontWeight.w800,
+                              ),
+                            ),
+                            SizedBox(height: 5.0),
+                            Text(
+                              furniture.furnitureDescription,
+                              style: GoogleFonts.questrial(
+                                fontSize: 16.0,
+                                color: Colors.grey,
+                              ),
+                            )
+                          ],
+                        ))
+                    .toList(),
+              ),
             ),
-          ),
-          CustomButton(),
-        ],
+            CustomButton(),
+          ],
+        ),
       ),
     );
   }
